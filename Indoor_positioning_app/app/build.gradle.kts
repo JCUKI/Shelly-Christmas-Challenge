bcfde348e31679026de038e156f2d812387d6d27
DESKTOP-DCVMFO1\Jernej <jernej.cukjati@gmail.com>
2023-12-21 20:34:30 +0100

    +Add TouchImageView and GeoPackage Library

--- Indoor_positioning_app/app/build.gradle.kts
+++ Indoor_positioning_app/app/build.gradle.kts
@@ -32,7 +32,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.github.MikeOrtiz:TouchImageView:1.4.1")
+    implementation("mil.nga.geopackage:geopackage-android:6.7.3")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
