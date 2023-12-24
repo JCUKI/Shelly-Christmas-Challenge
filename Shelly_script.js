72ffa6935049fc0e0070b70ea86ef1e5c171c0ba
DESKTOP-DCVMFO1\Jernej <jernej.cukjati@gmail.com>
2023-12-24 14:02:07 +0100

    *Modify Shelly script to publish rssi

--- Shelly_script.js
+++ Shelly_script.js
@@ -1,3 +1,16 @@
-Timer.set(1000, true, function() {
-MQTT.publish("ShellyTopic", "test1", 1, false)
-});
+let SCAN_DURATION = BLE.Scanner.INFINITE_SCAN;
+let ACTIVE_SCAN = true;
+
+SHELLY_BLU_CACHE = {}
+function scanCB(ev, res) {
+  if (ev !== BLE.Scanner.SCAN_RESULT) return;
+  
+  // skip if we have already found this device
+  if (typeof SHELLY_BLU_CACHE[res.addr] !== 'undefined') return;
+  
+      
+  SHELLY_BLU_CACHE[res.addr] = res.local_name;
+  MQTT.publish("ShellyTopic", res.rssi.toString(), 1, false)  
+}
+
+BLE.Scanner.Start({ duration_ms: SCAN_DURATION, active: true }, scanCB);
