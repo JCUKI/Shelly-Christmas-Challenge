ed6915c7e752fe52e9ae5081dc88a67241fe5ad7
DESKTOP-DCVMFO1\Jernej <jernej.cukjati@gmail.com>
2023-12-25 20:46:23 +0100

    *Update Shelly script

--- Shelly_script.js
+++ Shelly_script.js
@@ -1,16 +1,39 @@
+//Location specific constants
+//Shelly in the bedroom
+X = 1075
+Y = 0
+FLOOR = 2
+
+//let info = Shelly.getDeviceInfo();
+//console.log(info);
+
 let SCAN_DURATION = BLE.Scanner.INFINITE_SCAN;
 let ACTIVE_SCAN = true;
 
-SHELLY_BLU_CACHE = {}
 function scanCB(ev, res) {
   if (ev !== BLE.Scanner.SCAN_RESULT) return;
+
+  //if we didnt find Shelly device
+  if (typeof res.local_name !== 'string') return;
+  if (res.local_name.indexOf("Shelly") < 0) return;
+          
+  let sendString = res.addr + "##";
   
-  // skip if we have already found this device
-  if (typeof SHELLY_BLU_CACHE[res.addr] !== 'undefined') return;
+  //send source location
+  sendString += X.toString()+ "##" + Y.toString() + "##" + FLOOR.toString()+ "##";
   
-      
-  SHELLY_BLU_CACHE[res.addr] = res.local_name;
-  MQTT.publish("ShellyTopic", res.rssi.toString(), 1, false)  
+  /*if (typeof res.local_name !== 'string')//in case other devices are used 
+  {
+    sendString += "undefined##"; 
+  }
+  else*/  
+  {
+    sendString +=  res.local_name + "##"; 
+  } 
+  sendString +=  res.rssi.toString(); 
+    
+  MQTT.publish("ShellyTopic", sendString, 1, false)  
+  //console.log(sendString);
 }
 
 BLE.Scanner.Start({ duration_ms: SCAN_DURATION, active: true }, scanCB);
